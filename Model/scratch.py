75ba5cc9ad79b6bff8a0628cd6ce39057c6b3ed8
William_Junhao <jw3668@columbia.edu>
2019-10-24 21:32:30 -0400

    add save vertices

--- Model/scratch.py
+++ Model/scratch.py
@@ -1,18 +1,29 @@
 import matplotlib.pyplot as plt
 import pathlib as ph
 import numpy as np
+# import OpenEXR as exr
 
 BASE_PATH = ph.Path(__file__)
 
 DATA_PATH = BASE_PATH.parent.parent.joinpath('data')
+
+#check depth map
 all_png = list(DATA_PATH.glob('**/*.png'))
 print(all_png)
 
-depth = all_png[0]
+depth = all_png[-1]
 file = plt.imread(str(depth))
-print(str(depth))
-print(file.shape)
 
-plt.show()
-print(file.max())
-print(file.min())
+print('path',str(depth))
+print('depth_shape',file.shape)
+print('depth_max',file.max())
+print('depth_min',file.min())
+
+#check intrinsics
+cam_intr = np.load(DATA_PATH.joinpath('camera-intrinsics.npy'))
+print('cam_intr',cam_intr)
+
+#check vertices
+all_npy = list(DATA_PATH.glob('**/*.vertices.npy'))
+vertices = np.load(all_npy[0])
+print('vertices',vertices)
